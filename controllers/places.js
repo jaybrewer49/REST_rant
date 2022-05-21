6ebc0254f532d2759b7202aaaeffc3b9bea5d794
Jay Brewer <97116059+jaybrewer49@users.noreply.github.com>
2022-05-21 13:40:10 -0400

    New places can added

--- controllers/places.js
+++ controllers/places.js
@@ -1,41 +1,32 @@
 const app = require('express').Router()
+const places = require('../models/places.js')
+
+  app.get('/', (req, res) => { 
+  res.render('places/index', { places })
+ })
+
+ app.post('/', (req, res) => {
+
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
 
 
-
-// GET /places
-app.get('/', (req, res) => {
-    let places = [{
-        name: 'Chritys',
-        city: 'Greenville',
-        state: 'NC',
-        cuisines: 'Euro Pub',
-        pic: 'https://img.restaurantguru.com/re4d-design-Christys-Euro-Pub.jpg'
-      }, {
-        name: 'Sup Dogs',
-        city: 'Greenville',
-        state: 'NC',
-        cuisines: 'Hot Dogs and Hamburgers',
-        pic: 'https://gray-witn-prod.cdn.arcpublishing.com/resizer/EBDQhlXbhsrXP9AnHHT0tQ8FSBs=/1200x675/smart/filters:quality(85)/cloudfront-us-east-1.images.arcpublishing.com/gray/MRA6OIUOYBPZHG2BCO4ZHA4K4I.jpg'
-      }]
-      
-      res.render('places/index', { places })
-
-  })
-
-  app.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
-  })
-  
   app.get('/new', (req, res) => {
       let newPage = [{        
       }]
       res.render('places/new', { newPage })
   })
-
-
-
-  
 
 module.exports = app
 
---
+++ models/places.js
@@ -0,0 +1,14 @@
+
+module.exports = [{
+    name: 'Chritys',
+    city: 'Greenville',
+    state: 'NC',
+    cuisines: 'Euro Pub',
+    pic: 'https://img.restaurantguru.com/re4d-design-Christys-Euro-Pub.jpg'
+  }, {
+    name: 'Sup Dogs',
+    city: 'Greenville',
+    state: 'NC',
+    cuisines: 'Hot Dogs and Hamburgers',
+    pic: 'https://gray-witn-prod.cdn.arcpublishing.com/resizer/EBDQhlXbhsrXP9AnHHT0tQ8FSBs=/1200x675/smart/filters:quality(85)/cloudfront-us-east-1.images.arcpublishing.com/gray/MRA6OIUOYBPZHG2BCO4ZHA4K4I.jpg'
+  }]
